bf439c510467e20a5acfffc50de7fa1c5d98edf4
Hardik Gohil <32103022+HardikGohilHLR@users.noreply.github.com>
2021-01-18 22:55:15 +0530

    project setup

---
+++ src/components/Country.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="ch_country">
+        
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'country',
+    props: {
+        
+    }
+}
+</script> 
---
+++ src/views/Home.vue
@@ -0,0 +1,27 @@
+<template>
+	<div class="home">
+		<h1>Home</h1>
+		<pre>{{ data }}</pre>
+	</div>
+</template>
+
+<script> 
+
+import axios from 'axios';
+
+export default {
+	name: 'home', 
+	data() {		
+		return  {
+			data: []
+		}
+	},
+	mounted() {
+		axios
+		.get('https://restcountries.eu/rest/v2/name/india')
+		.then(response => { 
+			this.data = response.data;
+		})
+	}
+}
+</script>
